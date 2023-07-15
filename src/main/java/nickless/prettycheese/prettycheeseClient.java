45979b6316dfadcfc347f717b3b121f60c21c826
Nickless <nicolbonnyface@gmail.com>
2023-07-15 22:18:40 +0200

    First base commit

---
+++ src/main/java/nickless/prettycheese/prettycheeseClient.java
@@ -0,0 +1,12 @@
+package nickless.prettycheese;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class prettycheeseClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient(){
+
+    }
+    
+}
---
+++ src/main/java/nickless/prettycheese/prettycheeseModDataGenerator.java
@@ -0,0 +1,11 @@
+package nickless.prettycheese;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class prettycheeseModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
