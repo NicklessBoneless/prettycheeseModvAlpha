43a60dfe1dda3db01f9147b6c694f564e56b7925
Nickless <nicolbonnyface@gmail.com>
2023-07-16 00:36:52 +0200

    Second version commit

---
+++ src/main/java/nickless/prettycheese/item/ModItems.java
@@ -0,0 +1,31 @@
+package nickless.prettycheese.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import nickless.prettycheese.prettycheese;
+
+public class ModItems {
+
+    public static final Item CHEESE = registerItem("cheese", new Item(new FabricItemSettings()));
+    public static final Item SALT = registerItem("salt", new Item(new FabricItemSettings()));
+
+    private static void addItemsToFoodDrinkItemGroup(FabricItemGroupEntries entries){
+        entries.add(CHEESE);
+        entries.add(SALT);
+    }
+    private static Item registerItem(String name,Item item){
+        return Registry.register(Registries.ITEM, new Identifier(prettycheese.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        prettycheese.LOGGER.info("Registering Mod items for" + prettycheese.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToFoodDrinkItemGroup);
+    }
+}
